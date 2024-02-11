115c105678c0fdbee6db1d3e82243beee226d368
NafehM <nafehm@hotmail.com>
2024-02-11 21:29:00 +0000

    Binding and 2 fragments are created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,26 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
 
+    buildFeatures {
+        viewBinding = true
+    }
+
+}
+val navVersion = "2.7.7" // creating a variable for navigation version
 dependencies {
+
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion") //adding the dependencies for the navigation
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
