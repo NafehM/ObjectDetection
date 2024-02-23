95f0c37ec5dd7d161f245ddfa8f3e4b4c5e3eca8
NafehM <nafehm@hotmail.com>
2024-02-23 18:13:51 +0000

    Added documentations and tidied up the code

--- app/src/main/java/com/nmm/objectdetectionapp/CameraFragment.kt
+++ app/src/main/java/com/nmm/objectdetectionapp/CameraFragment.kt
@@ -1,43 +1,43 @@
 package com.nmm.objectdetectionapp
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
 //import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import com.nmm.objectdetectionapp.databinding.FragmentCameraBinding
 
-import android.content.ContentValues
-import android.os.Build
-import android.provider.MediaStore
-import androidx.camera.core.ImageCapture
 //import androidx.camera.video.Recorder
 //import androidx.camera.video.Recording
 //import androidx.camera.video.VideoCapture
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
 //import androidx.camera.video.FallbackStrategy
 //import androidx.camera.video.MediaStoreOutputOptions
 //import androidx.camera.video.Quality
 //import androidx.camera.video.QualitySelector
 //import androidx.camera.video.VideoRecordEvent
 //import androidx.core.content.PermissionChecker
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.nmm.objectdetectionapp.databinding.FragmentCameraBinding
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -45,15 +45,17 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
-
-typealias LumaListener = (luma: Double) -> Unit
+    /** Defines a typealias for a callback function that receives
+     * a luminance value (luma) as a Double and returns nothing (Unit).
+     */
+    typealias LumaListener = (luma: Double) -> Unit
 /**
  * A simple [Fragment] subclass.
  * Use the [CameraFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class CameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -63,6 +65,20 @@ class CameraFragment : Fragment() {
     private var imageCapture: ImageCapture? = null
     private lateinit var cameraExecutor: ExecutorService
 
+    //to define a contract for requesting permission using registerForActivityResult
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val permissionGranted = permissions.entries.all { it.value }
+            if (permissionGranted) {
+                startCamera()
+            } else {
+                Toast.makeText(context,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+                requireActivity().finish()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,20 +99,26 @@ class CameraFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //To check if permission granted, starts the camera
         if (allPermissionsGranted()) {
             startCamera()
         } else {
-//            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-            requestPermissions()
+            //A call to launch the permission request
+            requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
         }
         // Set up the listeners for take photo and video capture buttons
         binding.captureBtn.setOnClickListener { takePhoto() }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
-
     }
 
-
+    /**
+    * Initializes and starts the camera with the necessary configurations for preview,
+    * image capture, and image analysis.
+    * The function selects the back camera as the default
+    * @throws Exception if the use case binding fails, indicating an issue with camera initialization.
+     */
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
         cameraProviderFuture.addListener({
@@ -132,8 +154,16 @@ class CameraFragment : Fragment() {
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
+    /**
+     * Captures Captures a photo using the current camera configuration and saves it
+     * to the device's external storage in the "Pictures/Object Detection-Images" directory.
+     * It supports JPEG images
+     *
+     * Upon successfully taking a photo, a toast message is displayed, and the URI of the saved
+     * image is logged. If an error occurs during the image capture process, an error message is
+     * logged.
+     */
     private fun takePhoto() {
-
         // Get a stable reference of the modifiable image capture use case
         val imageCapture = imageCapture ?: return
 
@@ -176,45 +206,28 @@ class CameraFragment : Fragment() {
         )
     }
 
-    private fun requestPermissions() {
-        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
-    }
+//    private fun requestPermissions() {
+//        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+//    }
 
+    /**
+     * Checks if all required permissions specified in the `REQUIRED_PERMISSIONS` array are granted.
+     *
+     * This function iterates through each permission in `REQUIRED_PERMISSIONS` and uses
+     * `ContextCompat.checkSelfPermission` to compare the current permission state against
+     * `PackageManager.PERMISSION_GRANTED`. It returns `true` if all permissions are granted,
+     * indicating the app has the necessary permissions to operate as expected. Otherwise,
+     * it returns `false`, signaling that at least one required permission is missing.
+     *
+     * @return Boolean indicating whether all required permissions are granted (`true`) or not (`false`).
+     */
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(context, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
-                requireActivity().finish()
-            }
-        }
-    }
-
-    //New
-    private val activityResultLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions())
-        { permissions ->
-            // Handle Permission granted/rejected
-            var permissionGranted = true
-            permissions.entries.forEach {
-                if (it.key in REQUIRED_PERMISSIONS && !it.value)
-                    permissionGranted = false
-            }
-            if (!permissionGranted) {
-                Toast.makeText(requireContext(),
-                    "Permission request denied",
-                    Toast.LENGTH_SHORT).show()
-            } else {
-                startCamera()
-            }
-        }
-
+    /**
+     * Cleans up resources and shuts down the camera executor when the view is destroyed.
+     */
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -239,10 +252,10 @@ class CameraFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-        private const val REQUEST_CODE_PERMISSIONS = 10
+//        private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = //New
             mutableListOf (
-                Manifest.permission.CAMERA,
+                Manifest.permission.CAMERA
 //                Manifest.permission.RECORD_AUDIO
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
