9b13d187d3f6cc0cdc3051d3e794629a45cea554
NafehM <nafehm@hotmail.com>
2024-03-17 03:35:36 +0000

    Some warning have been solved.

--- app/src/main/java/com/nmm/objectdetectionapp/ImagePreviewFragment.kt
+++ app/src/main/java/com/nmm/objectdetectionapp/ImagePreviewFragment.kt
@@ -10,8 +10,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -30,6 +28,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ImagePreviewFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+@Suppress("DEPRECATION")
 class ImagePreviewFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -63,7 +62,7 @@ class ImagePreviewFragment : Fragment() {
             }
         }
 
-        arguments?.let {
+        arguments?.let { it ->
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
             arguments?.let {
@@ -75,7 +74,7 @@ class ImagePreviewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentImagePreviewBinding.inflate(inflater, container, false)
         return  binding.root
